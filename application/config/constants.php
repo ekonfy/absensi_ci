7bc6c8c3519fab2a245df36acd1ac45a145049d1
ekonfy <ekonfy@gmail.com>
2024-10-13 21:11:56 +0700

    bugfix:ganti SMA

--- application/config/constants.php
+++ application/config/constants.php
@@ -14,7 +14,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 */
 defined('SHOW_DEBUG_BACKTRACE') or define('SHOW_DEBUG_BACKTRACE', TRUE);
 
-define('WEBNAME', 'Absensi Siswa Al Muhajirin');
+define('WEBNAME', 'Absensi Siswa SMA Al Muhajirin');
 $webname = 'thisisimyglobalvariable';
 /*
 
